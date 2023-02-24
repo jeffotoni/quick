7f784f21304c730f7d5f2cddf46e5f95fecfd86e
Renan de Andrade <renan.correa@engdb.com.br>
2023-02-24 15:03:08 -0300

    improve cors mw

--- middleware/cors/cors.go
+++ middleware/cors/cors.go
@@ -6,18 +6,59 @@ import (
 	"github.com/rs/cors"
 )
 
-type Cors struct {
-	handler *cors.Cors
+type Options struct {
+	// AllowedOrigins is a list of origins a cross-domain request can be executed from.
+	// If the special "*" value is present in the list, all origins will be allowed.
+	// An origin may contain a wildcard (*) to replace 0 or more characters
+	// (i.e.: http://*.domain.com). Usage of wildcards implies a small performance penalty.
+	// Only one wildcard can be used per origin.
+	// Default value is ["*"]
+	AllowedOrigins []string
+	// AllowOriginFunc is a custom function to validate the origin. It take the origin
+	// as argument and returns true if allowed or false otherwise. If this option is
+	// set, the content of AllowedOrigins is ignored.
+	AllowOriginFunc func(origin string) bool
+	// AllowOriginRequestFunc is a custom function to validate the origin. It takes the HTTP Request object and the origin as
+	// argument and returns true if allowed or false otherwise. If this option is set, the content of `AllowedOrigins`
+	// and `AllowOriginFunc` is ignored.
+	AllowOriginRequestFunc func(r *http.Request, origin string) bool
+	// AllowedMethods is a list of methods the client is allowed to use with
+	// cross-domain requests. Default value is simple methods (HEAD, GET and POST).
+	AllowedMethods []string
+	// AllowedHeaders is list of non simple headers the client is allowed to use with
+	// cross-domain requests.
+	// If the special "*" value is present in the list, all headers will be allowed.
+	// Default value is [] but "Origin" is always appended to the list.
+	AllowedHeaders []string
+	// ExposedHeaders indicates which headers are safe to expose to the API of a CORS
+	// API specification
+	ExposedHeaders []string
+	// MaxAge indicates how long (in seconds) the results of a preflight request
+	// can be cached
+	MaxAge int
+	// AllowCredentials indicates whether the request can include user credentials like
+	// cookies, HTTP authentication or client side SSL certificates.
+	AllowCredentials bool
+	// AllowPrivateNetwork indicates whether to accept cross-origin requests over a
+	// private network.
+	AllowPrivateNetwork bool
+	// OptionsPassthrough instructs preflight to let other potential next handlers to
+	// process the OPTIONS method. Turn this on if your application handles OPTIONS.
+	OptionsPassthrough bool
+	// Provides a status code to use for successful OPTIONS requests.
+	// Default value is http.StatusNoContent (204).
+	OptionsSuccessStatus int
+	// Debugging flag adds additional output to debug server side CORS issues
+	Debug bool
 }
 
-func New() *Cors {
-	return &Cors{
-		handler: cors.New(cors.Options{
-			AllowedOrigins: []string{"*"},
-		}),
+func New(options ...Options) func(next http.Handler) http.Handler {
+	op := cors.Options{}
+
+	if len(options) > 0 {
+		op = cors.Options(options[0])
 	}
-}
 
-func (c *Cors) Handler(next http.Handler) http.Handler {
-	return c.handler.Handler(next)
+	c := cors.New(op)
+	return c.Handler
 }
