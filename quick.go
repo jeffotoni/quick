42811aedb5e6ad421dd11f39363a7ccadc190d7d
jeffotoni <jeff.otoni@gmail.com>
2023-02-28 14:29:49 -0300

    algumas correcoes em ordem de structs

--- quick.go
+++ quick.go
@@ -17,12 +17,12 @@ import (
 type Ctx struct {
 	Response  http.ResponseWriter
 	Request   *http.Request
+	resStatus int
+	bodyByte  []byte
+	JsonStr   string
 	Headers   map[string][]string
 	Params    map[string]string
 	Query     map[string]string
-	bodyByte  []byte
-	JsonStr   string
-	resStatus int
 }
 
 type Route struct {
@@ -38,8 +38,8 @@ type Route struct {
 type ctxServeHttp struct {
 	Path      string
 	Params    string
-	ParamsMap map[string]string
 	Method    string
+	ParamsMap map[string]string
 }
 
 type Config struct {
