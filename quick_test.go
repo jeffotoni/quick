d5ce79c6c877fc6b24b8d47a8bb488049da57093
marcos-dev88 <mv.piesske@gmail.com>
2023-02-27 22:48:58 -0300

    updating test coverage

--- quick_test.go
+++ quick_test.go
@@ -274,6 +274,141 @@ func TestQuick_Post(t *testing.T) {
 	}
 }
 
+// cover     -> go test -v -count=1 -cover -failfast -run ^TestQuick_PostListen$
+// coverHTML -> go test -v -count=1 -failfast -cover -coverprofile=coverage.out -run ^TestQuick_PostListen$; go tool cover -html=coverage.out
+func TestQuick_PostListen(t *testing.T) {
+	type args struct {
+		route       string
+		wantCode    int
+		wantOut     string
+		isWantedErr bool
+		reqBody     []byte
+		reqHeaders  map[string]string
+	}
+
+	type myType struct {
+		Name string `json:"name"`
+		Age  int    `json:"age"`
+	}
+
+	testSuccessMockHandler := func(c *Ctx) {
+		c.Set("Content-Type", "application/json")
+		b, _ := io.ReadAll(c.Request.Body)
+		resp := concat.String(`"data":`, string(b))
+		c.Status(200)
+		c.SendString(resp)
+	}
+
+	testSuccessMockHandlerString := func(c *Ctx) {
+		c.Set("Content-Type", "application/json")
+		mt := new(myType)
+		if err := c.Body(mt); err != nil {
+			t.Errorf("error: %v", err)
+		}
+		b, _ := json.Marshal(mt)
+		resp := concat.String(`"data":`, string(b))
+		c.Status(200)
+		c.String(resp)
+	}
+
+	testSuccessMockHandlerBind := func(c *Ctx) {
+		c.Set("Content-Type", "application/json")
+		mt := new(myType)
+		if err := c.Bind(mt); err != nil {
+			t.Errorf("error: %v", err)
+		}
+		b, _ := json.Marshal(mt)
+		resp := concat.String(`"data":`, string(b))
+		c.Status(200)
+		c.String(resp)
+	}
+
+	r := New()
+	r.Group("/my/group")
+	r.Post("/test", testSuccessMockHandler)
+	r.Post("/tester/:p1", testSuccessMockHandler)
+	r.Post("/", testSuccessMockHandlerString)
+	r.Post("/bind", testSuccessMockHandlerBind)
+
+	tests := []struct {
+		name string
+		args args
+	}{
+		{
+			name: "success",
+			args: args{
+				route:       "/my/group/test",
+				wantCode:    200,
+				wantOut:     `"data":{"name":"jeff", "age":35}`,
+				isWantedErr: false,
+				reqBody:     []byte(`{"name":"jeff", "age":35}`),
+			},
+		},
+		{
+			name: "success_param",
+			args: args{
+				route:       "/my/group/tester/:p1",
+				wantCode:    200,
+				wantOut:     `"data":{"name":"jeff", "age":35}`,
+				isWantedErr: false,
+				reqBody:     []byte(`{"name":"jeff", "age":35}`),
+			},
+		},
+		{
+			name: "success_without_param",
+			args: args{
+				route:       "/my/group/",
+				wantCode:    200,
+				wantOut:     `"data":{"name":"jeff","age":35}`,
+				isWantedErr: false,
+				reqBody:     []byte(`{"name":"jeff","age":35}`),
+				reqHeaders:  map[string]string{"Content-Type": "application/json"},
+			},
+		},
+		{
+			name: "success_bind",
+			args: args{
+				route:       "/my/group/bind",
+				wantCode:    200,
+				wantOut:     `"data":{"name":"jeff","age":35}`,
+				isWantedErr: false,
+				reqBody:     []byte(`{"name":"jeff","age":35}`),
+				reqHeaders:  map[string]string{"Content-Type": "application/json"},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			data, err := r.QuickTestListen(QuickMockTestServer{
+				Client:  http.DefaultClient,
+				Port:    8000,
+				URI:     tt.args.route,
+				Method:  "POST",
+				Headers: tt.args.reqHeaders,
+				Body:    tt.args.reqBody,
+			})
+
+			if (!tt.args.isWantedErr) && err != nil {
+				t.Errorf("error: %v", err)
+				return
+			}
+
+			s := strings.TrimSpace(data.BodyStr())
+			if s != tt.args.wantOut {
+				t.Errorf("was suppose to return %s and %s come", tt.args.wantOut, data.BodyStr())
+				return
+			}
+
+			if tt.args.wantCode != data.StatusCode() {
+				t.Errorf("was suppose to return %d and %d come", tt.args.wantCode, data.StatusCode())
+				return
+			}
+
+			t.Logf("outputBody -> %v", data.BodyStr())
+		})
+	}
+}
+
 // cover     -> go test -v -count=1 -cover -failfast -run ^TestQuick_Put$
 // coverHTML -> go test -v -count=1 -failfast -cover -coverprofile=coverage.out -run ^TestQuick_Put$; go tool cover -html=coverage.out
 func TestQuick_Put(t *testing.T) {
