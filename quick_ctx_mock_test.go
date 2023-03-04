980d8971b5416bd745051c57d20ce950436ce4f5
marcos-dev88 <mv.piesske@gmail.com>
2023-03-04 15:43:43 -0300

    creating new mock config to xml

--- quick_ctx_mock_test.go
+++ quick_ctx_mock_test.go
@@ -7,7 +7,7 @@ import (
 // cover     ->  go test -v -count=1 -cover -failfast -run ^TestQuickMockCtx_Get$
 // coverHTML ->  go test -v -count=1 -failfast -cover -coverprofile=coverage.out -run ^TestQuickMockCtx_Get$; go tool cover -html=coverage.out
 func TestQuickMockCtx_Get(t *testing.T) {
-	t.Run("success", func(tt *testing.T) {
+	t.Run("success_json", func(tt *testing.T) {
 		test := func(c *Ctx) {
 			tt.Logf("params -> %v", c.Params)
 			tt.Logf("body -> %v", c.Request.Body)
@@ -25,7 +25,25 @@ func TestQuickMockCtx_Get(t *testing.T) {
 		test(c)
 	})
 
-	t.Run("fail", func(tt *testing.T) {
+	t.Run("success_xml", func(tt *testing.T) {
+		test := func(c *Ctx) {
+			tt.Logf("params -> %v", c.Params)
+			tt.Logf("body -> %v", c.Request.Body)
+			tt.Logf("query -> %v", c.Query)
+		}
+
+		c := new(Ctx)
+
+		params := map[string]string{"myParam": "myValue"}
+		handler := QuickMockCtxXML(c, params, ContentTypeTextXML)
+		err := handler.Get("/my/test?isTrue=true&some=isAGoodValue")
+		if err != nil {
+			tt.Errorf("error: %v", err)
+		}
+		test(c)
+	})
+
+	t.Run("fail_json", func(tt *testing.T) {
 
 		var c *Ctx
 
@@ -37,12 +55,23 @@ func TestQuickMockCtx_Get(t *testing.T) {
 		}
 	})
 
+	t.Run("fail_xml", func(tt *testing.T) {
+
+		var c *Ctx
+
+		params := map[string]string{"myParam": "myValue"}
+		handler := QuickMockCtxXML(c, params, "")
+		err := handler.Get("/my/test?isTrue=true&some=isAGoodValue")
+		if err == nil {
+			tt.Errorf("should return a error and %v come", err)
+		}
+	})
 }
 
 // cover     ->  go test -v -count=1 -cover -failfast -run ^TestQuickMockCtx_Post$
 // coverHTML ->  go test -v -count=1 -failfast -cover -coverprofile=coverage.out -run ^TestQuickMockCtx_Post$; go tool cover -html=coverage.out
 func TestQuickMockCtx_Post(t *testing.T) {
-	t.Run("success", func(tt *testing.T) {
+	t.Run("success_json", func(tt *testing.T) {
 		test := func(c *Ctx) {
 			tt.Logf("params -> %v", c.Params)
 			tt.Logf("body -> %v", c.Request.Body)
@@ -60,7 +89,7 @@ func TestQuickMockCtx_Post(t *testing.T) {
 		test(c)
 	})
 
-	t.Run("fail", func(tt *testing.T) {
+	t.Run("fail_json", func(tt *testing.T) {
 		var c *Ctx
 		params := map[string]string{"myParam": "myValue"}
 		handler := QuickMockCtxJSON(c, params)
@@ -71,12 +100,40 @@ func TestQuickMockCtx_Post(t *testing.T) {
 		}
 	})
 
+	t.Run("success_xml", func(tt *testing.T) {
+		test := func(c *Ctx) {
+			t.Logf("myParam -> %v", c.Param("myParam"))
+			t.Logf("body -> %v", c.Request.Body)
+			t.Logf("query -> %v", c.Query)
+		}
+
+		c := new(Ctx)
+
+		params := map[string]string{"myParam": "myValue"}
+		handler := QuickMockCtxXML(c, params, ContentTypeAppXML)
+		err := handler.Post("/my/test", []byte(`<data><id>1</id><name>Jeff</name></data>`))
+		if err != nil {
+			tt.Errorf("error: %v", err)
+		}
+		test(c)
+	})
+
+	t.Run("fail_xml", func(tt *testing.T) {
+		var c *Ctx
+		params := map[string]string{"myParam": "myValue"}
+		handler := QuickMockCtxXML(c, params, ContentTypeTextXML)
+		err := handler.Post("/my/test", []byte(`[{"id": 1, "name": "jeff"}]`))
+		if err == nil {
+			tt.Errorf("should return a error and %v come", err)
+			return
+		}
+	})
 }
 
 // cover     ->  go test -v -count=1 -cover -failfast -run ^TestQuickMockCtx_Put$
 // coverHTML ->  go test -v -count=1 -failfast -cover -coverprofile=coverage.out -run ^TestQuickMockCtx_Put$; go tool cover -html=coverage.out
 func TestQuickMockCtx_Put(t *testing.T) {
-	t.Run("success", func(tt *testing.T) {
+	t.Run("success_json", func(tt *testing.T) {
 		test := func(c *Ctx) {
 			t.Logf("myParam -> %v", c.Param("myParam"))
 			t.Logf("body -> %v", c.Request.Body)
@@ -94,7 +151,7 @@ func TestQuickMockCtx_Put(t *testing.T) {
 		test(c)
 	})
 
-	t.Run("fail", func(tt *testing.T) {
+	t.Run("fail_json", func(tt *testing.T) {
 		var c *Ctx
 		params := map[string]string{"myParam": "myValue"}
 		handler := QuickMockCtxJSON(c, params)
@@ -105,12 +162,40 @@ func TestQuickMockCtx_Put(t *testing.T) {
 		}
 	})
 
+	t.Run("success_xml", func(tt *testing.T) {
+		test := func(c *Ctx) {
+			t.Logf("myParam -> %v", c.Param("myParam"))
+			t.Logf("body -> %v", c.Request.Body)
+			t.Logf("query -> %v", c.Query)
+		}
+
+		c := new(Ctx)
+
+		params := map[string]string{"myParam": "myValue"}
+		handler := QuickMockCtxXML(c, params, ContentTypeAppXML)
+		err := handler.Put("/my/test", []byte(`<data><id>1</id><name>Jeff</name></data>`))
+		if err != nil {
+			tt.Errorf("error: %v", err)
+		}
+		test(c)
+	})
+
+	t.Run("fail_xml", func(tt *testing.T) {
+		var c *Ctx
+		params := map[string]string{"myParam": "myValue"}
+		handler := QuickMockCtxXML(c, params, ContentTypeTextXML)
+		err := handler.Put("/my/test", []byte(`[{"id": 1, "name": "jeff"}]`))
+		if err == nil {
+			tt.Errorf("should return a error and %v come", err)
+			return
+		}
+	})
 }
 
 // cover     ->  go test -v -count=1 -cover -failfast -run ^TestQuickMockCtx_Delete$
 // coverHTML ->  go test -v -count=1 -failfast -cover -coverprofile=coverage.out -run ^TestQuickMockCtx_Delete$; go tool cover -html=coverage.out
 func TestQuickMockCtx_Delete(t *testing.T) {
-	t.Run("success", func(tt *testing.T) {
+	t.Run("success_json", func(tt *testing.T) {
 		test := func(c *Ctx) {
 			tt.Logf("params -> %v", c.Params)
 			tt.Logf("body -> %v", c.Request.Body)
@@ -129,10 +214,29 @@ func TestQuickMockCtx_Delete(t *testing.T) {
 		test(c)
 	})
 
-	t.Run("fail", func(tt *testing.T) {
+	t.Run("success_xml", func(tt *testing.T) {
+		test := func(c *Ctx) {
+			tt.Logf("params -> %v", c.Params)
+			tt.Logf("body -> %v", c.Request.Body)
+			tt.Logf("query -> %v", c.Query)
+		}
+
+		c := new(Ctx)
+
+		params := map[string]string{"myParam": "myValue"}
+		handler := QuickMockCtxXML(c, params, "")
+		err := handler.Delete("/")
+		if err != nil {
+			tt.Errorf("error: %v", err)
+			return
+		}
+		test(c)
+	})
+
+	t.Run("fail_xml", func(tt *testing.T) {
 		var c *Ctx
 		params := map[string]string{"myParam": "myValue"}
-		handler := QuickMockCtxJSON(c, params)
+		handler := QuickMockCtxXML(c, params, ContentTypeAppXML)
 		err := handler.Delete("/my/test")
 		if err == nil {
 			tt.Errorf("should return a error and %v come", err)
