aa4a4947d3519d99597ee286e4f3afc544982c9b
marcos-dev88 <mv.piesske@gmail.com>
2023-03-12 14:15:26 -0300

    moving unit tests to ctx_test.go

--- ctx_test.go
+++ ctx_test.go
@@ -1,1 +1,678 @@
 package quick
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"reflect"
+	"testing"
+)
+
+func TestCtx_Bind(t *testing.T) {
+	type fields struct {
+		Response  http.ResponseWriter
+		Request   *http.Request
+		resStatus int
+		bodyByte  []byte
+		JsonStr   string
+		Headers   map[string][]string
+		Params    map[string]string
+		Query     map[string]string
+	}
+	type args struct {
+		v interface{}
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &Ctx{
+				Response:  tt.fields.Response,
+				Request:   tt.fields.Request,
+				resStatus: tt.fields.resStatus,
+				bodyByte:  tt.fields.bodyByte,
+				JsonStr:   tt.fields.JsonStr,
+				Headers:   tt.fields.Headers,
+				Params:    tt.fields.Params,
+				Query:     tt.fields.Query,
+			}
+			if err := c.Bind(tt.args.v); (err != nil) != tt.wantErr {
+				t.Errorf("Ctx.Bind() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestCtx_BodyParser(t *testing.T) {
+	type fields struct {
+		Response  http.ResponseWriter
+		Request   *http.Request
+		resStatus int
+		bodyByte  []byte
+		JsonStr   string
+		Headers   map[string][]string
+		Params    map[string]string
+		Query     map[string]string
+	}
+	type args struct {
+		v interface{}
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &Ctx{
+				Response:  tt.fields.Response,
+				Request:   tt.fields.Request,
+				resStatus: tt.fields.resStatus,
+				bodyByte:  tt.fields.bodyByte,
+				JsonStr:   tt.fields.JsonStr,
+				Headers:   tt.fields.Headers,
+				Params:    tt.fields.Params,
+				Query:     tt.fields.Query,
+			}
+			if err := c.BodyParser(tt.args.v); (err != nil) != tt.wantErr {
+				t.Errorf("Ctx.BodyParser() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestCtx_Param(t *testing.T) {
+	type fields struct {
+		Response  http.ResponseWriter
+		Request   *http.Request
+		resStatus int
+		bodyByte  []byte
+		JsonStr   string
+		Headers   map[string][]string
+		Params    map[string]string
+		Query     map[string]string
+	}
+	type args struct {
+		key string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   string
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &Ctx{
+				Response:  tt.fields.Response,
+				Request:   tt.fields.Request,
+				resStatus: tt.fields.resStatus,
+				bodyByte:  tt.fields.bodyByte,
+				JsonStr:   tt.fields.JsonStr,
+				Headers:   tt.fields.Headers,
+				Params:    tt.fields.Params,
+				Query:     tt.fields.Query,
+			}
+			if got := c.Param(tt.args.key); got != tt.want {
+				t.Errorf("Ctx.Param() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestCtx_Body(t *testing.T) {
+	type fields struct {
+		Response  http.ResponseWriter
+		Request   *http.Request
+		resStatus int
+		bodyByte  []byte
+		JsonStr   string
+		Headers   map[string][]string
+		Params    map[string]string
+		Query     map[string]string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   []byte
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &Ctx{
+				Response:  tt.fields.Response,
+				Request:   tt.fields.Request,
+				resStatus: tt.fields.resStatus,
+				bodyByte:  tt.fields.bodyByte,
+				JsonStr:   tt.fields.JsonStr,
+				Headers:   tt.fields.Headers,
+				Params:    tt.fields.Params,
+				Query:     tt.fields.Query,
+			}
+			if got := c.Body(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Ctx.Body() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestCtx_BodyString(t *testing.T) {
+	type fields struct {
+		Response  http.ResponseWriter
+		Request   *http.Request
+		resStatus int
+		bodyByte  []byte
+		JsonStr   string
+		Headers   map[string][]string
+		Params    map[string]string
+		Query     map[string]string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   string
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &Ctx{
+				Response:  tt.fields.Response,
+				Request:   tt.fields.Request,
+				resStatus: tt.fields.resStatus,
+				bodyByte:  tt.fields.bodyByte,
+				JsonStr:   tt.fields.JsonStr,
+				Headers:   tt.fields.Headers,
+				Params:    tt.fields.Params,
+				Query:     tt.fields.Query,
+			}
+			if got := c.BodyString(); got != tt.want {
+				t.Errorf("Ctx.BodyString() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestCtx_JSON(t *testing.T) {
+	type fields struct {
+		Response  http.ResponseWriter
+		Request   *http.Request
+		resStatus int
+		bodyByte  []byte
+		JsonStr   string
+		Headers   map[string][]string
+		Params    map[string]string
+		Query     map[string]string
+	}
+	type args struct {
+		v interface{}
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &Ctx{
+				Response:  tt.fields.Response,
+				Request:   tt.fields.Request,
+				resStatus: tt.fields.resStatus,
+				bodyByte:  tt.fields.bodyByte,
+				JsonStr:   tt.fields.JsonStr,
+				Headers:   tt.fields.Headers,
+				Params:    tt.fields.Params,
+				Query:     tt.fields.Query,
+			}
+			if err := c.JSON(tt.args.v); (err != nil) != tt.wantErr {
+				t.Errorf("Ctx.JSON() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestCtx_XML(t *testing.T) {
+	type fields struct {
+		Response  http.ResponseWriter
+		Request   *http.Request
+		resStatus int
+		bodyByte  []byte
+		JsonStr   string
+		Headers   map[string][]string
+		Params    map[string]string
+		Query     map[string]string
+	}
+	type args struct {
+		v interface{}
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &Ctx{
+				Response:  tt.fields.Response,
+				Request:   tt.fields.Request,
+				resStatus: tt.fields.resStatus,
+				bodyByte:  tt.fields.bodyByte,
+				JsonStr:   tt.fields.JsonStr,
+				Headers:   tt.fields.Headers,
+				Params:    tt.fields.Params,
+				Query:     tt.fields.Query,
+			}
+			if err := c.XML(tt.args.v); (err != nil) != tt.wantErr {
+				t.Errorf("Ctx.XML() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestCtx_writeResponse(t *testing.T) {
+	type fields struct {
+		Response  http.ResponseWriter
+		Request   *http.Request
+		resStatus int
+		bodyByte  []byte
+		JsonStr   string
+		Headers   map[string][]string
+		Params    map[string]string
+		Query     map[string]string
+	}
+	type args struct {
+		b []byte
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &Ctx{
+				Response:  tt.fields.Response,
+				Request:   tt.fields.Request,
+				resStatus: tt.fields.resStatus,
+				bodyByte:  tt.fields.bodyByte,
+				JsonStr:   tt.fields.JsonStr,
+				Headers:   tt.fields.Headers,
+				Params:    tt.fields.Params,
+				Query:     tt.fields.Query,
+			}
+			if err := c.writeResponse(tt.args.b); (err != nil) != tt.wantErr {
+				t.Errorf("Ctx.writeResponse() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestCtx_Byte(t *testing.T) {
+	type fields struct {
+		Response  http.ResponseWriter
+		Request   *http.Request
+		resStatus int
+		bodyByte  []byte
+		JsonStr   string
+		Headers   map[string][]string
+		Params    map[string]string
+		Query     map[string]string
+	}
+	type args struct {
+		b []byte
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &Ctx{
+				Response:  tt.fields.Response,
+				Request:   tt.fields.Request,
+				resStatus: tt.fields.resStatus,
+				bodyByte:  tt.fields.bodyByte,
+				JsonStr:   tt.fields.JsonStr,
+				Headers:   tt.fields.Headers,
+				Params:    tt.fields.Params,
+				Query:     tt.fields.Query,
+			}
+			if err := c.Byte(tt.args.b); (err != nil) != tt.wantErr {
+				t.Errorf("Ctx.Byte() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestCtx_SendString(t *testing.T) {
+	type fields struct {
+		Response  http.ResponseWriter
+		Request   *http.Request
+		resStatus int
+		bodyByte  []byte
+		JsonStr   string
+		Headers   map[string][]string
+		Params    map[string]string
+		Query     map[string]string
+	}
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &Ctx{
+				Response:  tt.fields.Response,
+				Request:   tt.fields.Request,
+				resStatus: tt.fields.resStatus,
+				bodyByte:  tt.fields.bodyByte,
+				JsonStr:   tt.fields.JsonStr,
+				Headers:   tt.fields.Headers,
+				Params:    tt.fields.Params,
+				Query:     tt.fields.Query,
+			}
+			if err := c.SendString(tt.args.s); (err != nil) != tt.wantErr {
+				t.Errorf("Ctx.SendString() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestCtx_String(t *testing.T) {
+	type fields struct {
+		Response  http.ResponseWriter
+		Request   *http.Request
+		resStatus int
+		bodyByte  []byte
+		JsonStr   string
+		Headers   map[string][]string
+		Params    map[string]string
+		Query     map[string]string
+	}
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &Ctx{
+				Response:  tt.fields.Response,
+				Request:   tt.fields.Request,
+				resStatus: tt.fields.resStatus,
+				bodyByte:  tt.fields.bodyByte,
+				JsonStr:   tt.fields.JsonStr,
+				Headers:   tt.fields.Headers,
+				Params:    tt.fields.Params,
+				Query:     tt.fields.Query,
+			}
+			if err := c.String(tt.args.s); (err != nil) != tt.wantErr {
+				t.Errorf("Ctx.String() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestCtx_SendFile(t *testing.T) {
+	type fields struct {
+		Response  http.ResponseWriter
+		Request   *http.Request
+		resStatus int
+		bodyByte  []byte
+		JsonStr   string
+		Headers   map[string][]string
+		Params    map[string]string
+		Query     map[string]string
+	}
+	type args struct {
+		file []byte
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &Ctx{
+				Response:  tt.fields.Response,
+				Request:   tt.fields.Request,
+				resStatus: tt.fields.resStatus,
+				bodyByte:  tt.fields.bodyByte,
+				JsonStr:   tt.fields.JsonStr,
+				Headers:   tt.fields.Headers,
+				Params:    tt.fields.Params,
+				Query:     tt.fields.Query,
+			}
+			if err := c.SendFile(tt.args.file); (err != nil) != tt.wantErr {
+				t.Errorf("Ctx.SendFile() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestCtx_Set(t *testing.T) {
+	type fields struct {
+		Response  http.ResponseWriter
+		Request   *http.Request
+		resStatus int
+		bodyByte  []byte
+		JsonStr   string
+		Headers   map[string][]string
+		Params    map[string]string
+		Query     map[string]string
+	}
+	type args struct {
+		key   string
+		value string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &Ctx{
+				Response:  tt.fields.Response,
+				Request:   tt.fields.Request,
+				resStatus: tt.fields.resStatus,
+				bodyByte:  tt.fields.bodyByte,
+				JsonStr:   tt.fields.JsonStr,
+				Headers:   tt.fields.Headers,
+				Params:    tt.fields.Params,
+				Query:     tt.fields.Query,
+			}
+			c.Set(tt.args.key, tt.args.value)
+		})
+	}
+}
+
+func TestCtx_Append(t *testing.T) {
+	type fields struct {
+		Response  http.ResponseWriter
+		Request   *http.Request
+		resStatus int
+		bodyByte  []byte
+		JsonStr   string
+		Headers   map[string][]string
+		Params    map[string]string
+		Query     map[string]string
+	}
+	type args struct {
+		key   string
+		value string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantLen int
+	}{
+		{
+			name: "should be able to create a new header",
+			fields: fields{
+				Response: httptest.NewRecorder(),
+			},
+			args: args{
+				key:   "Append",
+				value: "one",
+			},
+			wantLen: 1,
+		},
+		{
+			name: "should be able to append to existing header",
+			fields: fields{
+				Response: func() http.ResponseWriter { x := httptest.NewRecorder(); x.Header().Set("Append", "one"); return x }(),
+			},
+			args: args{
+				key:   "Append",
+				value: "two",
+			},
+			wantLen: 2,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &Ctx{
+				Response:  tt.fields.Response,
+				Request:   tt.fields.Request,
+				resStatus: tt.fields.resStatus,
+				bodyByte:  tt.fields.bodyByte,
+				JsonStr:   tt.fields.JsonStr,
+				Headers:   tt.fields.Headers,
+				Params:    tt.fields.Params,
+				Query:     tt.fields.Query,
+			}
+			c.Append(tt.args.key, tt.args.value)
+
+			if len(c.Response.Header().Values(tt.args.key)) != tt.wantLen {
+				t.Errorf("c.Append(): want %v, got %v", tt.wantLen, len(c.Response.Header().Values(tt.args.key)))
+			}
+		})
+	}
+}
+
+func TestCtx_Accepts(t *testing.T) {
+	type fields struct {
+		Response  http.ResponseWriter
+		Request   *http.Request
+		resStatus int
+		bodyByte  []byte
+		JsonStr   string
+		Headers   map[string][]string
+		Params    map[string]string
+		Query     map[string]string
+	}
+	type args struct {
+		acceptType string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   *Ctx
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &Ctx{
+				Response:  tt.fields.Response,
+				Request:   tt.fields.Request,
+				resStatus: tt.fields.resStatus,
+				bodyByte:  tt.fields.bodyByte,
+				JsonStr:   tt.fields.JsonStr,
+				Headers:   tt.fields.Headers,
+				Params:    tt.fields.Params,
+				Query:     tt.fields.Query,
+			}
+			if got := c.Accepts(tt.args.acceptType); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Ctx.Accepts() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestCtx_Status(t *testing.T) {
+	type fields struct {
+		Response  http.ResponseWriter
+		Request   *http.Request
+		resStatus int
+		bodyByte  []byte
+		JsonStr   string
+		Headers   map[string][]string
+		Params    map[string]string
+		Query     map[string]string
+	}
+	type args struct {
+		status int
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   *Ctx
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &Ctx{
+				Response:  tt.fields.Response,
+				Request:   tt.fields.Request,
+				resStatus: tt.fields.resStatus,
+				bodyByte:  tt.fields.bodyByte,
+				JsonStr:   tt.fields.JsonStr,
+				Headers:   tt.fields.Headers,
+				Params:    tt.fields.Params,
+				Query:     tt.fields.Query,
+			}
+			if got := c.Status(tt.args.status); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Ctx.Status() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
