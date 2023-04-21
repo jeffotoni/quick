3058c8d61d410555050e486d81ecd26888cc8864
marcos-dev88 <mv.piesske@gmail.com>
2023-04-21 00:06:49 -0300

    creating new type MsgUUID and helper functions

---
+++ middleware/msgid/mock.go
@@ -0,0 +1,9 @@
+package msgid
+
+import "net/http"
+
+var returnSuccessHandler = func(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		next.ServeHTTP(w, r)
+	})
+}
