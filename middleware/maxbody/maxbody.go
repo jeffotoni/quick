c902efd313aba67ee80b44764563dfaabd2ba97b
jeffotoni <jeff.otoni@gmail.com>
2023-04-26 22:17:37 -0300

    otimizacao maxbody

--- middleware/maxbody/maxbody.go
+++ middleware/maxbody/maxbody.go
@@ -1,6 +1,7 @@
 package maxbody
 
 import (
+	"fmt"
 	"net/http"
 )
 
@@ -8,13 +9,14 @@ const DefaultMaxBytes int64 = 1024 * 1024 * 5
 
 func New(maxBytes ...int64) func(http.Handler) http.Handler {
 	mb := DefaultMaxBytes
-	if len(maxBytes) > 0 && maxBytes != nil {
+	if len(maxBytes) > 0 {
 		mb = maxBytes[0]
 	}
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			if r.ContentLength > mb {
-				http.Error(w, "Request body too large", http.StatusRequestEntityTooLarge)
+				w.WriteHeader(http.StatusRequestEntityTooLarge)
+				fmt.Fprint(w, "Request body too large")
 				return
 			}
 			r.Body = http.MaxBytesReader(w, r.Body, mb)
