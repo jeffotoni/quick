13ffbab000804cd94a03c1ce5dfc3bea29752fd3
marcos-dev88 <mv.piesske@gmail.com>
2023-05-06 22:09:06 -0300

    removing unused files

--- httpclient/get.go
+++
@@ -1,9 +0,0 @@
-package httpclient
-
-func Get(url string) *ClientResponse {
-	return defaultClient.Get(url)
-}
-
-func (c *Client) Get(url string) *ClientResponse {
-	return c.createRequest(url, "GET", nil)
-}
