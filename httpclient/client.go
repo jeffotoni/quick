3dbbc50bf3ef939a81b6ae44696ee3209e354ac4
marcos-dev88 <mv.piesske@gmail.com>
2023-05-06 21:06:22 -0300

    creating v2 to http_get_client

--- httpclient/client.go
+++ httpclient/client.go
@@ -3,57 +3,26 @@ package httpclient
 import (
 	"context"
 	"crypto/tls"
+	"io"
 	"net/http"
-	"time"
 )
 
-type (
-	HTTPClient interface {
-		Get(URL string) *ClientResponse
-	}
-
-	httpGoClient interface {
-		Do(request *http.Request) (*http.Response, error)
-	}
-)
-
-type (
-	HttpClient struct {
-		Ctx        context.Context
-		ClientHttp httpGoClient
-		Headers    map[string]string
-	}
-
-	ClientGoConfig struct {
-		Transport *http.Transport
-		Timeout   time.Duration
-	}
-)
-
-func New(hc ...HttpClient) HTTPClient {
-	conf := HttpClient{
-		Ctx:        context.Background(),
-		ClientHttp: ClientSec,
-	}
-
-	if len(hc) > 0 {
-		conf = hc[0]
-	}
-
-	return &conf
+type httpGoClient interface {
+	Do(request *http.Request) (*http.Response, error)
 }
 
-func NewGoClientConfig(cgc ...ClientGoConfig) httpGoClient {
-	conf := ClientSec
+type Client struct {
+	Ctx        context.Context
+	ClientHttp httpGoClient
+	Headers    map[string]string
+}
 
-	if len(cgc) > 0 {
-		conf = &http.Client{
-			Transport: cgc[0].Transport,
-			Timeout:   cgc[0].Timeout,
-		}
-	}
-
-	return conf
+var defaultClient = Client{
+	Ctx:        context.Background(),
+	ClientHttp: ClientInsec,
+	Headers: map[string]string{
+		"Content-Type": "application/json",
+	},
 }
 
 type ClientResponse struct {
@@ -66,9 +35,9 @@ var (
 	ClientInsec httpGoClient = &http.Client{
 		Transport: &http.Transport{
 			DisableKeepAlives:   true,
-			MaxIdleConns:        1000,
-			MaxConnsPerHost:     1000,
-			MaxIdleConnsPerHost: 1000,
+			MaxIdleConns:        10,
+			MaxConnsPerHost:     10,
+			MaxIdleConnsPerHost: 10,
 			TLSClientConfig: &tls.Config{
 				InsecureSkipVerify: true,
 			},
@@ -78,12 +47,46 @@ var (
 	ClientSec httpGoClient = &http.Client{
 		Transport: &http.Transport{
 			DisableKeepAlives:   true,
-			MaxIdleConns:        1000,
-			MaxConnsPerHost:     1000,
-			MaxIdleConnsPerHost: 1000,
+			MaxIdleConns:        10,
+			MaxConnsPerHost:     10,
+			MaxIdleConnsPerHost: 10,
 			TLSClientConfig: &tls.Config{
 				InsecureSkipVerify: false,
 			},
 		},
 	}
 )
+
+func (c *Client) createRequest(url, method string, requestBody io.Reader) *ClientResponse {
+
+	req, err := http.NewRequestWithContext(c.Ctx, method, url, requestBody)
+
+	if err != nil {
+		return &ClientResponse{Error: err}
+	}
+
+	if err != nil {
+		return &ClientResponse{Error: err}
+	}
+
+	for k, v := range c.Headers {
+		req.Header.Set(k, v)
+	}
+
+	resp, err := c.ClientHttp.Do(req)
+
+	if err != nil {
+		return &ClientResponse{Error: err}
+	}
+
+	defer resp.Body.Close()
+	code := resp.StatusCode
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return &ClientResponse{StatusCode: code, Error: err}
+
+	}
+
+	return &ClientResponse{Body: body, StatusCode: code, Error: err}
+}
