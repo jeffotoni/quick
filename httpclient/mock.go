de2574915c00297936cd7fcc82dff977de936309
marcos-dev88 <mv.piesske@gmail.com>
2023-05-08 21:20:20 -0300

    updating return mocks

--- httpclient/mock.go
+++ httpclient/mock.go
@@ -1,6 +1,9 @@
 package httpclient
 
-import "net/http"
+import (
+	"bytes"
+	"net/http"
+)
 
 type httpMock struct {
 	response *http.Response
@@ -9,4 +12,13 @@ type httpMock struct {
 
 func (h *httpMock) Do(*http.Request) (*http.Response, error) {
 	return h.response, h.err
+}
+
+var (
+	letsgoquickOutMock = `<html>  <head>    <title>Quick - Go</title>  </head>  <body>    <br/>    <br/>    <br/>    <br/>    <h1 style="text-align: center;">Quick - route 100% net/http</h1>  </body></html>`
+)
+
+func removeSpaces(b *[]byte) {
+	*b = bytes.ReplaceAll(*b, []byte("\t"), []byte(""))
+	*b = bytes.ReplaceAll(*b, []byte("\n"), []byte(""))
 }
