8616e8b34ca0eaafd873c175d40fe73e73572e26
marcos-dev88 <mv.piesske@gmail.com>
2023-05-08 21:20:05 -0300

    updating client tests and function call

--- httpclient/client.go
+++ httpclient/client.go
@@ -28,7 +28,6 @@ var defaultClient = Client{
 type ClientResponse struct {
 	Body       []byte
 	StatusCode int
-	Error      error
 }
 
 var (
@@ -58,49 +57,49 @@ var (
 )
 
 // Global request Calls
-func Get(url string) *ClientResponse {
+func Get(url string) (*ClientResponse, error) {
 	return defaultClient.Get(url)
 }
 
-func Post(url string, body io.Reader) *ClientResponse {
+func Post(url string, body io.Reader) (*ClientResponse, error) {
 	return defaultClient.Post(url, body)
 }
 
-func Put(url string, body io.Reader) *ClientResponse {
+func Put(url string, body io.Reader) (*ClientResponse, error) {
 	return defaultClient.Put(url, body)
 }
 
-func Delete(url string) *ClientResponse {
+func Delete(url string) (*ClientResponse, error) {
 	return defaultClient.Delete(url)
 }
 
 // Client request Calls
-func (c *Client) Get(url string) *ClientResponse {
+func (c *Client) Get(url string) (*ClientResponse, error) {
 	return c.createRequest(url, "GET", nil)
 }
 
-func (c *Client) Post(url string, body io.Reader) *ClientResponse {
+func (c *Client) Post(url string, body io.Reader) (*ClientResponse, error) {
 	return c.createRequest(url, "POST", body)
 }
 
-func (c *Client) Put(url string, body io.Reader) *ClientResponse {
+func (c *Client) Put(url string, body io.Reader) (*ClientResponse, error) {
 	return c.createRequest(url, "PUT", body)
 }
 
-func (c *Client) Delete(url string) *ClientResponse {
+func (c *Client) Delete(url string) (*ClientResponse, error) {
 	return c.createRequest(url, "DELETE", nil)
 }
 
-func (c *Client) createRequest(url, method string, requestBody io.Reader) *ClientResponse {
+func (c *Client) createRequest(url, method string, requestBody io.Reader) (*ClientResponse, error) {
 
 	req, err := http.NewRequestWithContext(c.Ctx, method, url, requestBody)
 
 	if err != nil {
-		return &ClientResponse{Error: err}
+		return nil, err
 	}
 
 	if err != nil {
-		return &ClientResponse{Error: err}
+		return nil, err
 	}
 
 	for k, v := range c.Headers {
@@ -110,7 +109,7 @@ func (c *Client) createRequest(url, method string, requestBody io.Reader) *Clien
 	resp, err := c.ClientHttp.Do(req)
 
 	if err != nil {
-		return &ClientResponse{Error: err}
+		return nil, err
 	}
 
 	defer resp.Body.Close()
@@ -118,9 +117,9 @@ func (c *Client) createRequest(url, method string, requestBody io.Reader) *Clien
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return &ClientResponse{StatusCode: code, Error: err}
+		return nil, err
 
 	}
 
-	return &ClientResponse{Body: body, StatusCode: code, Error: err}
+	return &ClientResponse{Body: body, StatusCode: code}, nil
 }
