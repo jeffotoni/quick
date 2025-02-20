c9d263c880b89845630593ab4b916d5af5c93119
Jaqueline Abreu <jaquelineabreuvr@gmail.com>
2025-02-20 16:51:48 -0300

    docs: adiciona documentação godoc para os códigos de status HTTP no http.status

--- quick_test.go
+++ quick_test.go
@@ -255,6 +255,19 @@ func ExampleGroup_Delete() {
 	// Out put: User deleted
 }
 
+// This function is named ExampleStatusText()
+// It is used as an example for Godoc
+func ExampleStatusText() {
+	fmt.Println(StatusText(200))
+	fmt.Println(StatusText(404))
+	fmt.Println(StatusText(500))
+
+	// Out put:
+	// OK
+	// Not Found
+	// Internal Server Error
+}
+
 // go test -v -run ^TestExampleGetDefaultConfig
 func TestExampleGetDefaultConfig(t *testing.T) {
 	expected := Config{
@@ -624,5 +637,24 @@ func TestGroup_Delete(t *testing.T) {
 	expectedBody := "User deleted"
 	if res.BodyStr() != expectedBody {
 		t.Errorf("Expected body '%s', but got '%s'", expectedBody, res.BodyStr())
+	}
+}
+
+// go test -v -run ^TestStatusText
+func TestStatusText(t *testing.T) {
+	if StatusText(200) != "OK" {
+		t.Errorf("Expected 'OK', but got '%s'", StatusText(200))
+	}
+
+	if StatusText(404) != "Not Found" {
+		t.Errorf("Expected 'Not Found', but got '%s'", StatusText(404))
+	}
+
+	if StatusText(500) != "Internal Server Error" {
+		t.Errorf("Expected 'Internal Server Error', but got '%s'", StatusText(500))
+	}
+
+	if StatusText(999) != "" {
+		t.Errorf("Expected empty string for unknown status code, but got '%s'", StatusText(999))
 	}
 }
