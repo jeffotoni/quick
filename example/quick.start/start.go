9c746f32aa537c7580e234aa26f60fecbe9cd1bd
Jaqueline Abreu <jaquelineabreuvr@gmail.com>
2025-02-27 15:30:29 -0300

    docs:add comments on start and apply default in readme

--- example/quick.start/start.go
+++ example/quick.start/start.go
@@ -3,21 +3,26 @@ package main
 import "github.com/jeffotoni/quick"
 
 func main() {
-	q := quick.New()
+	q := quick.New() // Initializes the Quick framework
+
+	// Sets a GET route for "/v1/user"
 	q.Get("/v1/user", func(c *quick.Ctx) error {
-		c.Set("Content-Type", "application/json")
-		return c.Status(200).SendString("Quick in action com Cors❤️!")
+		c.Set("Content-Type", "application/json")                       // Sets the content type as JSON
+		return c.Status(200).SendString("Quick in action with Cors❤️!") // Returns a success message
 	})
 
+	// Sets a GET route for "/v2"
 	q.Get("/v2", func(c *quick.Ctx) error {
-		c.Set("Content-Type", "application/json")
-		return c.Status(200).SendString("Está no ar️!")
+		c.Set("Content-Type", "application/json")         // Sets the content type as JSON
+		return c.Status(200).SendString("Is in the air!") // Returns a message indicating that the service is active
 	})
 
+	// Sets a GET route for "/v3"
 	q.Get("/v3", func(c *quick.Ctx) error {
-		c.Set("Content-Type", "application/json")
-		return c.Status(200).SendString("Rodando️!")
+		c.Set("Content-Type", "application/json")   // Sets the content type as JSON
+		return c.Status(200).SendString("Running!") // Returns a message confirming that the server is running
 	})
 
-	q.Listen("0.0.0.0:8080")
+	// Starts the server on port 8080, allowing connections from any IP
+	q.Listen("0.0.0.0.0.0:8080")
 }
