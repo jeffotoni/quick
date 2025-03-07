43961a3599273700b5416e7f9a8547a1710d3c5f
jeffotoni <jeff.otoni@gmail.com>
2025-03-07 10:30:43 -0300

    regex bug fix

--- example/quick.regex/quick.regex.go
+++ example/quick.regex/quick.regex.go
@@ -12,10 +12,10 @@ func main() {
 	q.Use(msgid.New())
 
 	// Corrected route using :id instead of {id:[0-9]+}
-	q.Get("/v1/user/:id", func(c *quick.Ctx) error {
+	q.Get("/v1/user/{id:[0-9]+}", func(c *quick.Ctx) error {
 		c.Set("Content-Type", "application/json")
 		return c.Status(200).String("Quick ação total!!!")
 	})
 
-	q.Listen(":8080")
+	q.Listen("0.0.0.0:8080")
 }
