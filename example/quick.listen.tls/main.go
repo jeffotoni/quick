8e2257b21b0b68d4be5bc68f7c514883b1a740cc
Jaqueline Abreu <jaquelineabreuvr@gmail.com>
2025-03-07 17:34:18 -0300

    docs: adds TSL example in the readmes

--- example/quick.listen.tls/main.go
+++ example/quick.listen.tls/main.go
@@ -2,17 +2,31 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/jeffotoni/quick"
 )
 
 func main() {
-	// start quick
+	// Initialize Quick instance
 	q := quick.New()
 
-	// instance Listen
-	fmt.Println("Run Server port:403")
+	// Print a message indicating that the server is starting on port 443
+	fmt.Println("Run Server port:443")
+
+	// Start the HTTPS server with TLS encryption
+	// - The server will listen on port 443
+	// - cert.pem: SSL/TLS certificate file
+	// - key.pem: Private key file for SSL/TLS encryption
 	err := q.ListenTLS(":443", "cert.pem", "key.pem")
 	if err != nil {
-		fmt.Printf("error when trying to connect with TLS: %v\n", err)
+		// Log an error message if the server fails to start
+		fmt.Printf("Error when trying to connect with TLS: %v\n", err)
 	}
 }
+
+//**Note for Linux Users**
+//By default, this example runs on **port 443**, which is a privileged port (below 1024).
+//On **Linux**, running on this port requires **superuser privileges**.
+
+//To run this example on Linux, use:
+//sudo go run main.go
