551a279a3d560afec9e3580cfa2a3d75a1d69225
Jaqueline Abreu <jaquelineabreuvr@gmail.com>
2025-03-11 11:07:31 -0300

    fix:cors

---
+++ example/middleware/cors/cors.no.middleware/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/jeffotoni/quick"
+)
+
+func main() {
+	app := quick.New()
+
+	app.Static("/", "./static")
+
+	app.Get("/v1/user", func(c *quick.Ctx) error {
+		c.Set("Content-Type", "application/json")
+
+		my := struct {
+			Name string `json:"name"`
+			Year int    `json:"year"`
+		}{
+			Name: "Teste",
+			Year: 2024,
+		}
+
+		fmt.Println("Enviando resposta:", my)
+		return c.Status(200).JSON(my)
+	})
+
+	log.Fatal(app.Listen("0.0.0.0:8080"))
+}
