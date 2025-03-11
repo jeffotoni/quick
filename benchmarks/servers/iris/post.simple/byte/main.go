5afdf5d8517c302527e7f48a026daf3bd947f942
Jaqueline Abreu <jaquelineabreuvr@gmail.com>
2025-03-11 16:52:16 -0300

    feat: adds test cases

---
+++ benchmarks/servers/iris/post.simple/byte/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/kataras/iris/v12"
+)
+
+// Struct representing a user model
+type My struct {
+	Name string `json:"name"` // User's name
+	Year int    `json:"year"` // User's birth year
+}
+
+func main() {
+	i := iris.New()
+
+	i.Post("/v1/user", func(ctx iris.Context) {
+
+		if _, err := ctx.GetBody(); err != nil {
+			ctx.JSON(iris.Map{"error": err.Error()})
+			return
+		}
+		ctx.JSON(http.StatusOK)
+
+	})
+
+	log.Fatal(i.Listen(":8080"))
+}
