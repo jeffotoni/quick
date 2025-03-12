378a65502c77d1c41e7cc497cda6ba7096357cb6
jeffotoni <jeff.otoni@gmail.com>
2025-03-11 22:05:44 -0300

    First stress tests on Quick, remembering that we are still in the development phase and have not released a release yet. And we did tests with a post using a very small JSON, to warm up. We used JSON Parse in the request and a JSON response. And we measured using k6.

--- benchmarks/servers/fiber/post.simples/byte/main.go
+++ benchmarks/servers/fiber/post.simples/byte/main.go
@@ -4,20 +4,17 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+// $ curl --location 'http://localhost:8080/v1/user' \
+// --header 'Content-Type: application/json' \
+// --data '{"name": "Alice", "year": 20}'
 func main() {
 	app := fiber.New()
 
 	app.Post("/v1/user", func(c *fiber.Ctx) error {
 		c.Set("Content-Type", "application/json")
-
 		rawBody := c.Body()
 		return c.Send(rawBody)
-
 	})
 
 	app.Listen(":8080")
 }
-
-//curl --location 'http://localhost:8080/v1/user' \
-// --header 'Content-Type: application/json' \
-// --data '{"name": "Alice", "year": 20}'
--- benchmarks/servers/gin/post.simple/byte/main.go
+++ benchmarks/servers/gin/post.simple/byte/main.go
@@ -13,19 +13,20 @@ type My struct {
 	Year int    `json:"year"` // User's birth year
 }
 
+// $ curl --location 'http://localhost:8080/v1/user' \
+// --header 'Content-Type: application/json' \
+// --data '{"name": "Alice", "year": 20}'
 func main() {
 
 	gin.SetMode(gin.ReleaseMode)
 
-	// r := gin.Default()
 	r := gin.New()
 
 	r.POST("/v1/user", func(c *gin.Context) {
 		var my My // Create a variable to store incoming user data
-
 		_, err := io.ReadAll(c.Request.Body)
 		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "Erro ao ler o body"})
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 			return
 		}
 
@@ -35,7 +36,3 @@ func main() {
 
 	r.Run(":8080")
 }
-
-//curl --location 'http://localhost:8080/v1/user' \
-// --header 'Content-Type: application/json' \
-// --data '{"name": "Alice", "year": 20}'
