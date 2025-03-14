7de01b2f1207e83e9afe6531e17d7084bfca8ae5
jeffotoni <jeff.otoni@gmail.com>
2025-03-14 09:59:51 -0300

    Removing go.mod and go.sum from subdirectories and updating gitignore

--- benchmarks/servers/fiber/post.simples/bodyparser/main.go
+++ benchmarks/servers/fiber/post.simples/bodyparser/main.go
@@ -1,37 +1,37 @@
 package main
 
-import (
-	"github.com/gofiber/fiber/v2"
-)
+// import (
+// 	"github.com/gofiber/fiber/v2"
+// )
 
-// Struct representing a user model
-type My struct {
-	Name string `json:"name"` // User's name
-	Year int    `json:"year"` // User's birth year
-}
+// // Struct representing a user model
+// type My struct {
+// 	Name string `json:"name"` // User's name
+// 	Year int    `json:"year"` // User's birth year
+// }
 
-func main() {
-	app := fiber.New()
+// func main() {
+// 	app := fiber.New()
 
-	app.Post("/v1/user", func(c *fiber.Ctx) error {
-		c.Set("Content-Type", "application/json")
-		// Parse the request body into the struct
-		var my My // Create a variable to store incoming user data
+// 	app.Post("/v1/user", func(c *fiber.Ctx) error {
+// 		c.Set("Content-Type", "application/json")
+// 		// Parse the request body into the struct
+// 		var my My // Create a variable to store incoming user data
 
-		// Parse the request body into the struct
-		err := c.BodyParser(&my)
-		if err != nil {
-			// If parsing fails, return a 400 Bad Request response
-			return c.Status(400).SendString(err.Error())
-		}
+// 		// Parse the request body into the struct
+// 		err := c.BodyParser(&my)
+// 		if err != nil {
+// 			// If parsing fails, return a 400 Bad Request response
+// 			return c.Status(400).SendString(err.Error())
+// 		}
 
-		// Return the parsed JSON data as a response with 200 OK
-		return c.Status(200).JSON(my)
+// 		// Return the parsed JSON data as a response with 200 OK
+// 		return c.Status(200).JSON(my)
 
-	})
+// 	})
 
-	app.Listen(":8080")
-}
+// 	app.Listen(":8080")
+// }
 
 //curl --location 'http://localhost:8080/v1/user' \
 // --header 'Content-Type: application/json' \
