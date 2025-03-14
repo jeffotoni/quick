51b1f5fbb2767af09c4050867057562514e4160c
jeffotoni <jeff.otoni@gmail.com>
2025-03-14 09:59:51 -0300

    Removing go.mod and go.sum from subdirectories and updating gitignore

--- benchmarks/servers/gin/post.simple/shouldBindBodyWithJSON/main.go
+++ benchmarks/servers/gin/post.simple/shouldBindBodyWithJSON/main.go
@@ -1,40 +1,40 @@
 package main
 
-import (
-	"net/http"
+// import (
+// 	"net/http"
 
-	"github.com/gin-gonic/gin"
-)
+// 	"github.com/gin-gonic/gin"
+// )
 
-// Struct representing a user model
-type My struct {
-	Name string `json:"name"` // User's name
-	Year int    `json:"year"` // User's birth year
-}
+// // Struct representing a user model
+// type My struct {
+// 	Name string `json:"name"` // User's name
+// 	Year int    `json:"year"` // User's birth year
+// }
 
-// $ curl --location 'http://localhost:8080/v1/user' \
-// --header 'Content-Type: application/json' \
-// --data '{"name": "Alice", "year": 20}'
-func main() {
+// // $ curl --location 'http://localhost:8080/v1/user' \
+// // --header 'Content-Type: application/json' \
+// // --data '{"name": "Alice", "year": 20}'
+// func main() {
 
-	gin.SetMode(gin.ReleaseMode)
+// 	gin.SetMode(gin.ReleaseMode)
 
-	// r := gin.Default()
-	r := gin.New()
+// 	// r := gin.Default()
+// 	r := gin.New()
 
-	r.POST("/v1/user", func(c *gin.Context) {
-		var my My // Create a variable to store incoming user data
+// 	r.POST("/v1/user", func(c *gin.Context) {
+// 		var my My // Create a variable to store incoming user data
 
-		// Parse the request body into the struct
-		err := c.ShouldBindBodyWithJSON(&my)
-		if err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
-		}
+// 		// Parse the request body into the struct
+// 		err := c.ShouldBindBodyWithJSON(&my)
+// 		if err != nil {
+// 			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+// 			return
+// 		}
 
-		// Return the parsed JSON data as a response with 200 OK
-		c.JSON(http.StatusOK, my)
-	})
+// 		// Return the parsed JSON data as a response with 200 OK
+// 		c.JSON(http.StatusOK, my)
+// 	})
 
-	r.Run(":8080")
-}
+// 	r.Run(":8080")
+// }
--- benchmarks/servers/iris/post.simple/byte/main.go
+++ benchmarks/servers/iris/post.simple/byte/main.go
@@ -1,30 +1,30 @@
 package main
 
-import (
-	"log"
+// import (
+// 	"log"
 
-	"github.com/kataras/iris/v12"
-)
+// 	"github.com/kataras/iris/v12"
+// )
 
-// $ curl --location 'http://localhost:8080/v1/user' \
-// --header 'Content-Type: application/json' \
-// --data '{"name": "Alice", "year": 20}'
-func main() {
-	app := iris.New()
+// // $ curl --location 'http://localhost:8080/v1/user' \
+// // --header 'Content-Type: application/json' \
+// // --data '{"name": "Alice", "year": 20}'
+// func main() {
+// 	app := iris.New()
 
-	app.Post("/v1/user", func(ctx iris.Context) {
-		// Capture the request body as `[]byte`
-		bodyBytes, err := ctx.GetBody()
-		if err != nil {
-			ctx.StatusCode(iris.StatusInternalServerError)
-			ctx.WriteString("Error reading body")
-			return
-		}
+// 	app.Post("/v1/user", func(ctx iris.Context) {
+// 		// Capture the request body as `[]byte`
+// 		bodyBytes, err := ctx.GetBody()
+// 		if err != nil {
+// 			ctx.StatusCode(iris.StatusInternalServerError)
+// 			ctx.WriteString("Error reading body")
+// 			return
+// 		}
 
-		// Returns the same bytes received in the response
-		ctx.ContentType("application/json")
-		ctx.Write(bodyBytes)
-	})
+// 		// Returns the same bytes received in the response
+// 		ctx.ContentType("application/json")
+// 		ctx.Write(bodyBytes)
+// 	})
 
-	log.Fatal(app.Listen(":8080"))
-}
+// 	log.Fatal(app.Listen(":8080"))
+// }
