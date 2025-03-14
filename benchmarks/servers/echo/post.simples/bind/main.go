36ef2d6821e670e9b175da7482139b263b14228e
jeffotoni <jeff.otoni@gmail.com>
2025-03-14 09:59:51 -0300

    Removing go.mod and go.sum from subdirectories and updating gitignore

--- benchmarks/servers/echo/post.simples/bind/main.go
+++ benchmarks/servers/echo/post.simples/bind/main.go
@@ -1,36 +1,36 @@
 package main
 
-import (
-	"net/http"
+// import (
+// 	"net/http"
 
-	"github.com/labstack/echo/v4"
-)
+// 	"github.com/labstack/echo/v4"
+// )
 
-// Struct representing a user model
-type My struct {
-	Name string `json:"name"` // User's name
-	Year int    `json:"year"` // User's birth year
-}
+// // Struct representing a user model
+// type My struct {
+// 	Name string `json:"name"` // User's name
+// 	Year int    `json:"year"` // User's birth year
+// }
 
-func main() {
-	e := echo.New()
+// func main() {
+// 	e := echo.New()
 
-	// Define a POST route at /v1/user
+// 	// Define a POST route at /v1/user
 
-	e.POST("/v1/user", func(c echo.Context) error {
-		var my My
+// 	e.POST("/v1/user", func(c echo.Context) error {
+// 		var my My
 
-		if err := c.Bind(&my); err != nil {
+// 		if err := c.Bind(&my); err != nil {
 
-			return c.String(http.StatusBadRequest, err.Error())
-		}
+// 			return c.String(http.StatusBadRequest, err.Error())
+// 		}
 
-		return c.JSON(http.StatusOK, my)
-	})
+// 		return c.JSON(http.StatusOK, my)
+// 	})
 
-	e.Start(":8080")
-}
+// 	e.Start(":8080")
+// }
 
-//curl --location 'http://localhost:8080/v1/user' \
-// --header 'Content-Type: application/json' \
-// --data '{"name": "Alice", "year": 20}'
+// //curl --location 'http://localhost:8080/v1/user' \
+// // --header 'Content-Type: application/json' \
+// // --data '{"name": "Alice", "year": 20}'
--- benchmarks/servers/fiber/post.simples/byte/main.go
+++ benchmarks/servers/fiber/post.simples/byte/main.go
@@ -1,20 +1,20 @@
 package main
 
-import (
-	"github.com/gofiber/fiber/v2"
-)
+// import (
+// 	"github.com/gofiber/fiber/v2"
+// )
 
-// $ curl --location 'http://localhost:8080/v1/user' \
-// --header 'Content-Type: application/json' \
-// --data '{"name": "Alice", "year": 20}'
-func main() {
-	app := fiber.New()
+// // $ curl --location 'http://localhost:8080/v1/user' \
+// // --header 'Content-Type: application/json' \
+// // --data '{"name": "Alice", "year": 20}'
+// func main() {
+// 	app := fiber.New()
 
-	app.Post("/v1/user", func(c *fiber.Ctx) error {
-		c.Set("Content-Type", "application/json")
-		rawBody := c.Body()
-		return c.Send(rawBody)
-	})
+// 	app.Post("/v1/user", func(c *fiber.Ctx) error {
+// 		c.Set("Content-Type", "application/json")
+// 		rawBody := c.Body()
+// 		return c.Send(rawBody)
+// 	})
 
-	app.Listen(":8080")
-}
+// 	app.Listen(":8080")
+// }
