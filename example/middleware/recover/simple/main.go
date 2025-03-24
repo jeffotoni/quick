610e461e4a2e1f3648c7a557db18c1de4b09ead6
emmadal <emmanueldalougou@gmail.com>
2025-03-24 16:50:13 +0000

    docs: add doc for recover middleware

---
+++ example/middleware/recover/simple/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"errors"
+
+	"github.com/jeffotoni/quick"
+	"github.com/jeffotoni/quick/middleware/recover"
+)
+
+func main() {
+	q := quick.New()
+
+	// Apply the Recover middleware
+	q.Use(recover.New(recover.Config{
+		App: q,
+	}))
+
+	// Define a test route
+	q.Get("/v1/panic", func(c *quick.Ctx) error {
+		c.Set("Content-Type", "application/json")
+
+		// halt the server
+		panic(errors.New("Panicking!"))
+	})
+
+	// Start the server
+	q.Listen("0.0.0.0:8080")
+}
