cc6545a8cde463e9294062bab58f397cb2b540c2
Jaqueline Abreu <jaquelineabreuvr@gmail.com>
2025-03-27 17:07:19 -0300

    docs: more adjust example ctx

--- example/quick.start/start.go
+++ example/quick.start/start.go
@@ -7,20 +7,20 @@ func main() {
 
 	// Sets a GET route for "/v1/user"
 	q.Get("/v1/user", func(c *quick.Ctx) error {
-		c.Set("Content-Type", "application/json")                       // Sets the content type as JSON
-		return c.Status(200).SendString("Quick in action with Cors❤️!") // Returns a success message
+		c.Set("Content-Type", "application/json")       // Sets the content type as JSON
+		return c.Status(200).String("Quick in action!") // Returns a success message
 	})
 
 	// Sets a GET route for "/v2"
 	q.Get("/v2", func(c *quick.Ctx) error {
-		c.Set("Content-Type", "application/json")         // Sets the content type as JSON
-		return c.Status(200).SendString("Is in the air!") // Returns a message indicating that the service is active
+		c.Set("Content-Type", "application/json")     // Sets the content type as JSON
+		return c.Status(200).String("Is in the air!") // Returns a message indicating that the service is active
 	})
 
 	// Sets a GET route for "/v3"
 	q.Get("/v3", func(c *quick.Ctx) error {
-		c.Set("Content-Type", "application/json")   // Sets the content type as JSON
-		return c.Status(200).SendString("Running!") // Returns a message confirming that the server is running
+		c.Set("Content-Type", "application/json") // Sets the content type as JSON
+		return c.Status(200).String("Running!")   // Returns a message confirming that the server is running
 	})
 
 	// Starts the server on port 8080, allowing connections from any IP
