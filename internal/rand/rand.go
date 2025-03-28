37809745178ca1444c30e74436a59c6b7b201876
jeffotoni <jeff.otoni@gmail.com>
2025-03-28 16:23:57 -0300

    Example cusom middleware quick net/http native

--- internal/rand/rand.go
+++ internal/rand/rand.go
@@ -1,8 +1,10 @@
 package rand
 
 import (
-	"crypto/rand"
+	randx "crypto/rand"
 	"math/big"
+	randm "math/rand"
+	"time"
 )
 
 func RandomInt(min, max int) (int, error) {
@@ -11,7 +13,7 @@ func RandomInt(min, max int) (int, error) {
 	diffBigInt := new(big.Int).Sub(maxBigInt, minBigInt)
 
 	randomBytes := make([]byte, diffBigInt.BitLen()/8+1)
-	_, err := rand.Read(randomBytes)
+	_, err := randx.Read(randomBytes)
 	if err != nil {
 		return 0, err
 	}
@@ -20,4 +22,20 @@ func RandomInt(min, max int) (int, error) {
 	randomInt.Mod(randomInt, diffBigInt)
 	randomInt.Add(randomInt, minBigInt)
 	return int(randomInt.Int64()), nil
+}
+
+// Generate Trace ID with letters and numbers, 100% stdlib
+func TraceID() string {
+	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	const length = 16
+
+	// Initialize the seed once per run
+	src := randm.NewSource(time.Now().UnixNano())
+	r := randm.New(src)
+
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[r.Intn(len(charset))]
+	}
+	return string(b)
 }
